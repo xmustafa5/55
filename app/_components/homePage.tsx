ca3419a9c87a01a1c6b5c76b91ddb4a6667287b3
Mustafa mohammed <107437940+xmustafa5@users.noreply.github.com>
2025-11-01 11:35:14 +0300

    fsd

--- app/_components/homePage.tsx
+++ app/_components/homePage.tsx
@@ -1,11 +1,12 @@
-import { BoxDomnaSvg, CheckenSvg, CreativitySvg, FiftyFiveLanguageSvg, FlowerSvg, HighFiveSvg, noiseSvg } from "@/public/svg/mainSvg";
+import { BoxDomnaSvg, boxFiftyfive, CheckenSvg, CreativitySvg, FiftyFiveLanguageSvg, FlowerSvg, HighFiveSvg, noiseSvg } from "@/public/svg/mainSvg";
 import Image from "next/image";
 
 function HomePage() {
     return (
-        <div className="flex flex-col justify-center items-center overflow-hidden w-full h-screen p-[175px] px-[181px] bg-[#001487] [background-image:linear-gradient(0deg,rgba(255,255,255,0.04)_0%,rgba(255,255,255,0.04)_100%)]">
+        <div className="flex w-[100%] h-[796px] py-[175px] px-[181px] flex-col justify-center items-center bg-gradient-to-b from-[#0F218D] to-[#0F218D] bg-opacity-[.96]">
             {noiseSvg}
-            <div className="relative flex flex-col items-center w-[1079px] h-[247px] gap-10 shrink-0">
+            <div className="flex relative w-[400] sm:w-[500] md:w-[700px] lg:w-[900px] 2xl:w-[1079px] py-16 flex-col items-center gap-10 rounded-[32px] bg-white/4">
+
                 <div className="flex flex-col items-center gap-2">
                     <h1 className="text-[#A6ADD5] text-center text-[40px] md:text-[56px] lg:text-[72px] font-semibold leading-[90%]">
                         Creativity Our
@@ -19,15 +20,10 @@ function HomePage() {
                 <p className="w-[350px] md:w-[612.688px] text-white text-center text-[18px] font-normal leading-[130%]">
                     we’re a creative agency that shakes the dice with bold ideas, sharp strategy, and a team that knows how to win. we throw confident that is exactly what your brand needs
                 </p>
-                {CheckenSvg}
-                {BoxDomnaSvg}
-                {FlowerSvg}
-                {HighFiveSvg}
-                {CreativitySvg}
-                {/* {FiftyFiveLanguageSvg} */}
-                <div className=" absolute w-42 h-42 left-[121px] top-[-68px] rotate-[-10deg]">
-                    <Image src={"/images/Group.png"} alt="Group" fill />
+                <div className="flex z-10 p-[18.949px] flex-col absolute right-1/2 translate-x-1/2 -top-18 gap-[9.475px]">
+                    {boxFiftyfive}
                 </div>
+
             </div>
         </div>
     );
